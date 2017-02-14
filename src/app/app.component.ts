d80ee47956ee3c3025ae95779831844837bdd6ba
Jaganathan <jjaganathan@deloitte.com>
2017-02-13 21:12:18 -0500

    2nd round

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,17 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  template: `
+  <h1>Hello {{name}}</h1>
+  <fieldset>
+    
+    <img [src]="image" />
+  </fieldset>
+  <label [style.color]='color'>Favourite Color: </label> {{color}}
+  `,
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent { 
+  name = 'Alex Smith'; 
+  image="favicon.ico";
+  color = 'red';
+}
