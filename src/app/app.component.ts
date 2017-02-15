e398c96655cf0ac09dd62e7df03fba68ab19ef87
Jaganathan <jjaganathan@deloitte.com>
2017-02-15 07:03:50 -0500

    added additional code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,10 +9,51 @@ import { Component } from '@angular/core';
     <img [src]="image" />
   </fieldset>
   <label [style.color]='color'>Favourite Color: </label> {{color}}
+  <button (click)='clicked()'>Toggle Color</button>
+  <div style='border: 1px solid red' [hidden]='hideAddress'>
+  <br />
+  Street: {{street}} 
+  <br/>
+  City: {{city}}
+</div>
+  <button (click)='addressClick()'>Show/Hide Address</button>
+
+  <br/>
+  <select #selector (change)='regionChange(selector.value)'>
+    <option>East</option>
+    <option>West</option>
+    <option>North</option>
+    <option>South</option>
+  </select>
+
+  <br/>
+  <p>{{currentRegion}}</p>
   `,
 })
 export class AppComponent { 
   name = 'Alex Smith'; 
   image="favicon.ico";
   color = 'red';
+  
+  currentRegion = '';
+
+  regionChange(region :string) 
+  {
+      this.currentRegion = region;
+  }
+
+  hideAddress = false;
+
+  addressClick() {
+    this.hideAddress = !this.hideAddress;
+  }
+
+  street = 'Appu Street';
+  city = 'New York';
+  region = '';
+
+
+  clicked() {
+    this.color = this.color == 'red' ? 'blue' : 'red';
+  }
 }
